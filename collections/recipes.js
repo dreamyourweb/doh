9192f09df9d8ca89d814cc37659a0d994b87ea25
jorisc <joriscuppen@gmail.com>
2014-09-20 23:12:37 +0200

    Stuffff!!!f111

--- collections/recipes.js
+++ collections/recipes.js
@@ -3,23 +3,30 @@ Recipes = new Meteor.Collection('recipes');
 Recipes.helpers({
   recommendedBeer: function() {
 
-    mapping = {
-    seafood: 	"lager, pilsner, stout, witbier",
-	meaty: 		"lager, ale, bock, porter, hefenweizen",
-	creamy: 	"witbier",
-	sweet: 		"fruit, hefenweizen",
-	salty: 		"pilsner, lager, stout",
-	spicy: 		"ale, fruit, lager, pilsner, bock",
-	oily: 		"lager, pilsner",
-	fruity: 	"fruit, hefenweizen, pilsner",
-	smoky: 		"porter, stout",
-	cheesy: 	"	",
-	eggy: 		"fruit",
-	roasty: 	"stout, bock",
-	crispy: 	"fruit",
-	rich: 		"stout, bock"};
+	mapping = {
+	seafood: 	["pilsner", "stout", "witbier"],
+	meaty: 		["pilsner", "bock", "hefenweizen"],
+	creamy: 	["witbier", "radler"],
+	sweet: 		["fruit", "hefenweizen"],
+	salty: 		["pilsner", "stout", "brown"],
+	spicy: 		["fruitig", "pilsner", "bock"],
+	oily: 		["pilsner", "radler"],
+	fruity: 	["fruit", "hefenweizen", "pilsner"],
+	smoky: 		["stout", "brown"],
+	cheesy: 	["fruit"],
+	eggy: 		["fruit"],
+	roasty: 	["stout", "bock", "brown"],
+	crispy: 	["fruit"],
+	rich: 		["stout", "bock", "brown", "radler"]};
 
-	return mapping[this.category[0]];
+
+	cats = [];
+	this.category.forEach(function(cat){
+		cats = _.shuffle(cats.concat(mapping[cat]));
+	});
+
+	cats = _.uniq(cats);
+	return _.str.capitalize(_.toSentence(cats));
 
   }
 });
